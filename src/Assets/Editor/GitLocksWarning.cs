b25b4f75b5e36943afb312302a27f590c55d80fd
Jamie Tucker <jamie@asteroidbase.com>
2019-08-04 16:13:28 -0400

    Added Unity project

---
+++ src/Assets/Editor/GitLocksWarning.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Diagnostics;
+
+public class GitLocksWarning : UnityEditor.AssetModificationProcessor {
+  private static string[] OnWillSaveAssets(string[] paths) {
+    WarningWindow.Init("OnWillSaveAssets");
+
+    ProcessStartInfo processStartInfo = new ProcessStartInfo(); 
+    processStartInfo.FileName = Application.dataPath+"/git-locks.sh";
+    // processStartInfo.FileName = System.IO.Path.GetFullPath("Packages/com.jamie-tucker.git-locks/git-locks.sh");
+
+    Process process = Process.Start(processStartInfo); 
+    string strOutput = process.StandardOutput.ReadToEnd(); 
+    process.WaitForExit(); 
+    UnityEngine.Debug.Log(strOutput);
+
+    foreach (string path in paths)
+      UnityEngine.Debug.Log(path);
+    return paths;
+  }
+}
---
+++ src/Assets/Editor/WarningWindow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class WarningWindow : EditorWindow
+{
+    private static string message;
+
+    public static void Init(string message)
+    {
+        WarningWindow.message = message;
+        WarningWindow window = (WarningWindow)EditorWindow.GetWindow(typeof(WarningWindow));
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label(WarningWindow.message, EditorStyles.boldLabel);
+    }
+}
